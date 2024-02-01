d56d141d30e403e90b307e8d4fc1e58ee4f1021b
Taylor Monacelli <taylormonacelli@gmail.com>
2024-01-31 19:46:48 -0800

    refactor

--- all.sh
+++ all.sh
@@ -5,19 +5,36 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 rm -rf $SCRIPT_DIR/scratch.????
 
-tmp2=$(mktemp -d $SCRIPT_DIR/scratch.XXXX)
+scratch=$(mktemp -d $SCRIPT_DIR/scratch.XXXX)
 
-cd $tmp2
+cd $scratch
 docker ps --format "{{.Names}}" --filter name='^/dagger-engine-*' | xargs --no-run-if-empty -I"{}" docker rm --force {}
 
-cd $tmp2
-tmp=$(mktemp -d $tmp2/create-react-app-tmp.XXXX)
+cd $scratch
+tmp=$(mktemp -d $scratch/create-react-app-tmp.XXXX)
 time npx create-react-app $tmp/my-app --template typescript
 tar cf $tmp/my-app.tar --exclude=node_modules -C $tmp/my-app .
 tar --list -f $tmp/my-app.tar
 
-cd $tmp2
+cd $scratch
 rm -f README.md
-tar xf $tmp/my-app.tar -C $tmp2
+tar xf $tmp/my-app.tar -C $scratch
 chmod +x *.sh
-bash -x ./e2e.sh
+
+cd $scratch
+dagger mod init --name=mymod --sdk=go
+git init
+git add -A
+git commit -am Boilerplate
+
+dagger mod install github.com/quartz-technology/daggerverse/node@9ce087b83aa8b85f828d7d92ce39bd7c055cfc0f
+
+cp main-step1.go.txt main.go
+dagger functions
+
+cp main-step2.go.txt main.go
+dagger functions
+
+cp main-step3.go.txt main.go
+dagger functions
+dagger call build
